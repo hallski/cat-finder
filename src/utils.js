70a324ad8d02c2528fbd1eba27a04ab59170b8e7
Mikael Hallendal <hallski@hallski.org>
2017-11-02 14:29:32 +0100

    Let any resolve to the input items
    Makes it better for chaining it to another method that needs the items.

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,12 @@
-// Returns a function taking items and returns a resolved promise if any item match the predicate. Otherwise a rejection.
-
 /**
- * Returns a function that takes an array of items and returns a promise that will resolve if any item match the predicate.
+ * Returns a function that takes an array of items and returns a promise that will resolve to the items
+ * if any item match the predicate.
  *
  * @param {Function} predicate
  */
 function any(predicate) {
   return function(items) {
-    return items.find(predicate) !== undefined ? Promise.resolve() : Promise.reject()
+    return items.find(predicate) !== undefined ? Promise.resolve(items) : Promise.reject()
   }
 }
 
